205acf1aff9bea1523f6f280d128d0384aa95ad6
Zach Budesa <zrbudesa42@gmail.com>
2023-08-09 10:19:30 -0500

    Updated prep code

--- src/01_preparation/download-compile-rpe-data.R
+++ src/01_preparation/download-compile-rpe-data.R
@@ -6,16 +6,13 @@
 library(qualtRics)
 library(tidyverse)
 
-View(
-all_surveys()
-)
 
 surveys <- all_surveys()
 
 qs <- paste0("Item",seq(1:14))
 
-rpe <- fetch_survey(surveyID = surveys$id[surveys$name == "Harm Reduction Scales - Response Process Evaluation"],
-                    include_questions = qs,
+rpe <- fetch_survey(surveyID = 
+                      surveys$id[surveys$name == "Harm Reduction Scales - Response Process Evaluation"],
                     force_request = TRUE)
 
 items <- survey_questions(surveys$id[surveys$name == "Harm Reduction Scales - Response Process Evaluation"])
