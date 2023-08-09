46a701a12b413cafc5a33711177c2eb271241b07
Zach Budesa <zrbudesa42@gmail.com>
2023-08-09 13:04:12 -0500

    Finalize code file for RPE cleaning and create clean file

--- src/01_preparation/download-compile-rpe-data.R
+++ src/01_preparation/download-compile-rpe-data.R
@@ -12,6 +12,7 @@ surveys <- all_surveys()
 # Actual Items
 item <- survey_questions(surveys$id[surveys$name == "Harm Reduction Scales - Response Process Evaluation"])
 
+# Pul question text, remove html, add question numbers
 items <- item %>% 
   filter(grepl("<h1 style=", question)) %>% 
   select(question) %>% 
@@ -31,11 +32,17 @@ rpe <- rpe %>%
 
 
 question <- rpe %>% 
+  # Add ID number
+  mutate(id = row_number())
+  # Select relevant columns
   select(id,
          starts_with(c("resp", "exp-resp", "Comp", "Para", "Comment"))) %>% 
   select(-ResponseId) %>% 
+  # Pivot all relevant columns
   pivot_longer(-id) %>% 
+  # Set item number from item column
   mutate(item = str_extract(name, "\\d+"),
+  # Rename so that all columns are the name and do not have item numbers
          name = 
            case_when(
              grepl("exp-resp", name) ~ "Explanation for Response",
@@ -44,17 +51,31 @@ question <- rpe %>%
              grepl("Comment", name) ~ "Comments",
              grepl("Comp", name) ~ "Comprehension"
            )) %>%
+  # Remove missing rows from items respondents didn't see
   filter(!is.na(value)) %>% 
+  # Pivot wide to combine columns
   pivot_wider(id_cols = c(item, id), names_from = name, values_from = value) %>% 
   arrange(desc(item)) %>% 
+  # Combine with question text
   left_join(items, by = c("item" = "number")) %>% 
+  # Add group assignements
   left_join(data.frame(Group = rep(1:5, length.out = 46), item = as.character(1:46))) %>% 
+  # Select & rename columns of interest
   select(`Participant ID` = id, 
          `Question Number` = item,
          `Question Text` = question,
          Response, `Explanation for Response`,
          `Item Paraphrase`, `Comprehension`,
          `Comments`, Group)
-  
+ 
+# Set sheet names for excel doc
+sheet_names <- list(`Group 1` = question[question$Group == 1,],
+                    `Group 2` = question[question$Group == 1,],
+                    `Group 3` = question[question$Group == 3,],
+                    `Group 4` = question[question$Group == 4,],
+                    `Group 5` = question[question$Group == 5,])
+
+# Write file
+openxlsx::write.xlsx(sheet_names, file = "data/clean/001_RPE-Data.xlsx") 
 
 
